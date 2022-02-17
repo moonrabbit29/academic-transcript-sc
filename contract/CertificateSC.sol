34216033a90e8349860d584e497983c8c8ca96e7
dendiaryar <dendiaryar@gmail.com>
2022-02-17 10:06:28 +0700

    feat: add smartcontract v1

---
+++ contract/CertificateSC.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract CertificateSC {
+    address payable public owner;
+
+    struct Transcript {
+        bytes32 transcript;
+        uint256 Timestamp;
+    }
+
+    constructor() payable {
+        owner = payable(msg.sender);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    mapping(bytes32 => Transcript[]) private studentToTranscript;
+
+    function is_transcript_exist(
+        bytes32 hash_data,
+        Transcript[] memory arrayTranscript
+    ) private pure returns (bool) {
+        for (uint256 i = 0; i < arrayTranscript.length; i++) {
+            if (arrayTranscript[i].transcript == hash_data) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function register_transcript(bytes32 transcriptHash, bytes32 studentId)
+        public
+        onlyOwner
+        returns (bool success)
+    {
+        if (studentToTranscript[studentId].length > 0) {
+            if (
+                is_transcript_exist(
+                    transcriptHash,
+                    studentToTranscript[studentId]
+                )
+            ) {
+                return false;
+            } else {
+                studentToTranscript[studentId].push(
+                    Transcript(transcriptHash, block.timestamp)
+                );
+            }
+        } else {
+            studentToTranscript[studentId].push(
+                Transcript(transcriptHash, block.timestamp)
+            );
+        }
+    }
+
+    function retrieve_student_transcript(bytes32 studentId)
+        public
+        view
+        onlyOwner
+        returns (Transcript[] memory)
+    {
+        return studentToTranscript[studentId];
+    }
+
+    function verify_certificate_transcript(bytes32 transcriptHash,bytes32 studentId)
+        public
+        view
+        returns (bool)
+    {
+        bool transcriptExist =  is_transcript_exist(
+                    transcriptHash,
+                    studentToTranscript[studentId]
+                );
+        return transcriptExist;
+    }
+}
