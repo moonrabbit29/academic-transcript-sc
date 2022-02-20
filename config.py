01ac3a74f696214e8e1f4eb899f76a0955640773
dendiaryar <dendiaryar@gmail.com>
2022-02-20 15:54:19 +0700

    feat: interact with sc in ganache, retrieve and register tc hash

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+
+
+class Config:
+    # later work on external db
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class ProductionConfig(Config):
+    FLASK_ENV = 'production'
+
+
+class DevelopmentConfig(Config):
+    FLASK_ENV = 'development'
+    DEVELOPMENT = True
+    DEBUG = True
+    ACCOUNT = os.environ.get("ACCOUNT_ADDRESS_DEV")
+    ACCOUNT_KEY = os.environ.get("PRIVATE_KEY_DEV")
+    ETHEREUM_ENDPOINT_URI = os.environ.get("ETHEREUM_ENDPOINT_URI_DEVELOPMENT")
+    SMART_CONTRACT_ADDRESS = os.environ.get("ETHEREUM_CONTRACT_ADDRESS_DEV")
+    CHAIN_ID = 1337
