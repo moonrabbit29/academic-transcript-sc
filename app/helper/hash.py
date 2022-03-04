3164f6f699a9d715a38893fd9d84e752d5ca26b1
Vanz-Noob <52117843+Vanz-Noob@users.noreply.github.com>
2022-03-04 15:54:12 +0700

    feat: adding keccak hashing method

--- app/helper/hash.py
+++ app/helper/hash.py
@@ -6,3 +6,12 @@ class Hash:
         s = hashlib.sha3_256()
         s.update(data)
         return s.hexdigest()
+
+# #if want to use keccak256 instead
+# import sha3
+
+# class Hash:
+#     def hash_data(self, data):
+#         k = sha3.keccak_256()
+#         k.update(data)
+#         return k.hexdigest()
